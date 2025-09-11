b3edf540dc6affe49cca9765436af31e775bf987
Amon Kumar <amondaurala2018@gmail.com>
2025-09-11 09:16:48 -0700

    Date:- 11-sept-2025

--- Winning_Number.py
+++ Winning_Number.py
@@ -1,3 +1,10 @@
+# Guess a Number
+# Make a variable, like winning_number and assign any to it.
+# Ask user to guess a number:
+#if user guessed correctly then print "! You Win !"
+# if user didn't guessed correctly then:
+    # if user guessed Higher than actual number then print "Too High"
+    # if user guessed lower than actual number then print "Too Low"
 winning_number = 25
 user_input = int(input("Gass a Number Game 1 to 100: "))
 if user_input == winning_number:
