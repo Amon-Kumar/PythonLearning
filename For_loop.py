c574d82319c683482077526d8a99742a5f1d0fd4
Amon Kumar <amondaurala2018@gmail.com>
2025-09-13 10:15:58 -0700

    Date:- 13-sept-2025

---
+++ For_loop.py
@@ -0,0 +1,15 @@
+# For loop in python
+# Syntax:- for variable in sequence:
+                # code block (Statement)
+# numbers print --> 1 to 10.
+for i in range(11):
+    print(i)
+
+print("***********************************")
+
+# Loop in List
+fruits = ['Apple',"Mango",'Grapess']
+for f in fruits:
+    print(f)
+
+
