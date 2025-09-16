97e14ccaef61b288671f0e66eb541811f2707c9f
Amon Kumar <amondaurala2018@gmail.com>
2025-09-16 11:28:53 -0700

    Date:- 16-sept-2025

---
+++ List.py
@@ -0,0 +1,9 @@
+myList = ['Amon','Aakash','Sumit','Shushank','Shubham']
+mylist=list(('Amon','Aakash','Sumit','Shushank','Shubham'))
+print(myList)
+print(len(myList))
+print(type(myList))
+print(type(mylist))
+print(mylist[1])
+print(mylist[:3])
+print(mylist[:-1])
